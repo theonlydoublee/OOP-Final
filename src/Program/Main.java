72e66e1e54d62dccace6c4895ae853c7f078c379
theonlydoublee <theonlydoublee@gmail.com>
2021-02-24 11:45:25 -0700

    added comments for plans

--- src/Program/Main.java
+++ src/Program/Main.java
@@ -3,6 +3,10 @@ package Program;
 public class Main {
 
     public static void main(String[] args) {
+    
+        //add menu and player creation
+        //also stat tracker
+    
         Play.Connect4.run("1", "2");
     }
 }
